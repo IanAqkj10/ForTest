7d5a9623495782d344a8d9d428dd8395ff719acf
ljh <314951570@qq.com>
2022-03-23 10:59:56 +0800

    123

---
+++ src/main/java/BB.java
@@ -0,0 +1,19 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+/**
+ * @Description:
+ * @author:ljh
+ * @date:2022/3/18 10:44
+ */
+
+@Data
+@AllArgsConstructor
+public class BB {
+
+    private BigDecimal bigDecimal;
+    private String name;
+
+}
