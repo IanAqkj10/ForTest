bd0cce1234af528a53afcb73c7617d74322a507c
ljh <314951570@qq.com>
2022-03-25 14:32:03 +0800

    v6

---
+++ src/main/java/TestA.java
@@ -0,0 +1,11 @@
+/**
+ * @Description:
+ * @author:ljh
+ * @date:2022/3/18 15:14
+ */
+
+public interface TestA {
+
+    public abstract void qaq();
+
+}
---
+++ src/main/java/TestClassA.java
@@ -0,0 +1,13 @@
+/**
+ * @Description:
+ * @author:ljh
+ * @date:2022/3/18 15:14
+ */
+
+public class TestClassA implements TestA{
+
+    @Override
+    public void qaq() {
+
+    }
+}
