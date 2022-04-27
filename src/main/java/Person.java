e95e943afc95ad56dd610ea6edc78090699392b1
ljh <314951570@qq.com>
2022-04-27 15:22:54 +0800

    1111

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,5 +1,7 @@
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 
 import java.util.Date;
 
@@ -7,10 +9,13 @@ import java.util.Date;
  * @author:IanJ
  * @date:2021/7/3 21:24
  */
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Accessors(chain = true)
 public class Person {
 
+    //xxxxxx
     private String name;
     private Integer age;
     private Date date;
@@ -27,37 +32,6 @@ public class Person {
         this.date = date;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-    public Long getQaq() {
-        return qaq;
-    }
-
-    public void setQaq(Long qaq) {
-        this.qaq = qaq;
-    }
 
     @Override
     public String toString() {
